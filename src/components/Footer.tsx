7173dbd2ea359d95f7d4cc58cf6d46836faf089b
wangbertha <wangbertha@gmail.com>
2024-09-16 15:46:49 -0500

    Create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      Designed in Figma | Built with TypeScript & React | Bertha Wang 2024
+    </footer>
+  );
+};
+
+export default Footer;
