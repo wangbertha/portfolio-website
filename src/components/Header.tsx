3f45cb63db8c0dd25dbc8f2ebb081eed2bcde311
wangbertha <wangbertha@gmail.com>
2024-09-23 13:49:09 -0500

    Add functionality to toggle nav for narrow screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import { Link, useLocation } from "react-router-dom";
 import "./Header.css";
+import { useState } from "react";
 
 const Header = () => {
   const { pathname } = useLocation();
+  const [menuToggle, setMenuToggle] = useState(false);
 
   return (
     <header>
@@ -12,17 +14,18 @@ const Header = () => {
             <Link to="">Bertha Wang</Link>
           </p>
           <svg
+            onClick={() => setMenuToggle(!menuToggle)}
             className="nav-hamburger-icon"
             viewBox="0 0 100 80"
             width="40"
             height="40"
           >
-            <rect y="20" width="75" height="10" rx="0.5rem"></rect>
-            <rect y="45" width="75" height="10" rx="0.5rem"></rect>
-            <rect y="70" width="75" height="10" rx="0.5rem"></rect>
+            <rect y="20" width="75" height="8" rx="0.5rem"></rect>
+            <rect y="45" width="75" height="8" rx="0.5rem"></rect>
+            <rect y="70" width="75" height="8" rx="0.5rem"></rect>
           </svg>
         </div>
-        <menu>
+        <menu className={menuToggle ? "shown" : "hidden"}>
           <li className={pathname === "/projects" ? "selected" : ""}>
             <Link to="projects">Projects</Link>
           </li>
