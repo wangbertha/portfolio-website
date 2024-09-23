831d1ad536edfe4b77ade0b4d6c5e1f8fed1d8ea
wangbertha <wangbertha@gmail.com>
2024-09-23 15:20:37 -0500

    Fix static asset handling for images

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,6 +1,6 @@
 const projects = [
   {
-    image: "/src/assets/project-fair-housing.png",
+    image: new URL("../assets/project-fair-housing.png", import.meta.url).href,
     imgAlt: "Screenshot of a dashboard application with rows of housing data",
     title: "Fair Housing Audit Dashboard",
     year: 2024,
@@ -8,7 +8,7 @@ const projects = [
       "Designed and implemented a dashboard for a Chicago nonprofit to uphold fair housing law compliance. Developed with React.JS and Node.JS (Express) and read/written to a PostgreSQL database. Deployed with Google App Engine and set up authentication with GCP Identity-Aware Proxy (IAP).",
     links: [
       {
-        icon: "/src/assets/github-mark.svg",
+        icon: new URL("../assets/github-mark.svg", import.meta.url).href,
         link: "https://github.com/wangbertha/hcv_auditor",
         alt: "GitHub logo",
       },
@@ -22,7 +22,7 @@ const projects = [
     ],
   },
   {
-    image: "/src/assets/project-ghost-buses.png",
+    image: new URL("../assets/project-ghost-buses.png", import.meta.url).href,
     imgAlt:
       "Screenshot of Chicago Ghost Buses homepage explaining 'What is a Ghost Bus?'",
     title: "Chicago Ghost Buses",
@@ -31,12 +31,12 @@ const projects = [
       "Open-source project under Chi Hack Night analyzing bus reliability in Chicago. Resolved frontend bugs and provided pull-request feedback for HTML/CSS and React.JS",
     links: [
       {
-        icon: "/src/assets/github-mark.svg",
+        icon: new URL("../assets/github-mark.svg", import.meta.url).href,
         link: "https://github.com/chihacknight/ghost-buses-frontend",
         alt: "GitHub logo",
       },
       {
-        icon: "/src/assets/ghost-bus-icon.png",
+        icon: new URL("../assets/ghost-bus-icon.png", import.meta.url).href,
         link: "https://ghostbuses.com/",
         alt: "Thumbnail of Ghost Buses website",
       },
@@ -45,7 +45,8 @@ const projects = [
     skillsMed: [],
   },
   {
-    image: "/src/assets/project-portfolio-website.png",
+    image: new URL("../assets/project-portfolio-website.png", import.meta.url)
+      .href,
     imgAlt:
       "Screenshot of a portfolio website that displays the person's role and career summary",
     title: "Portfolio Website",
@@ -54,12 +55,12 @@ const projects = [
       "Developed this portfolio website with React.JS. Designed with Figma.",
     links: [
       {
-        icon: "/src/assets/github-mark.svg",
+        icon: new URL("../assets/github-mark.svg", import.meta.url).href,
         link: "https://github.com/wangbertha/portfolio-website",
         alt: "GitHub logo",
       },
       {
-        icon: "/src/assets/figma-logo.svg",
+        icon: new URL("../assets/figma-logo.svg", import.meta.url).href,
         link: "https://www.figma.com/design/McXMjYuY17sXh4MO1dls7T/Portfolio-Website?node-id=0-1&t=CxbCRHQ8ysLcrZZB-1",
         alt: "Figma logo",
       },
