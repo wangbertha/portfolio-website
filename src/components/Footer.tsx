33fdbdff523021f80f1c83addc22a9094fd06696
wangbertha <wangbertha@gmail.com>
2024-12-23 20:15:25 -0600

    Add portfolio repo link to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,19 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer>
-      Designed in Figma | Built with TypeScript & React | Bertha Wang 2024
+      <a
+        href="https://github.com/wangbertha/portfolio-website"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Portfolio Repo{" "}
+        <img
+          src={new URL("../assets/github-mark-white.svg", import.meta.url).href}
+          alt=""
+          height={10}
+        />
+      </a>{" "}
+      | Designed in Figma | Built with TypeScript & React | Bertha Wang 2024
     </footer>
   );
 };
